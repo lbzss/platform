5d74204b07bf9a0fab209ad7c19d4d9b51d56b23
lbzs <627062293@qq.com>
2023-11-17 17:09:10 +0800

    github

--- src/.vuepress/.temp/pages/guide/index.html.vue
+++
@@ -1,8 +0,0 @@
-<template><div><h3 id="平台工程理论基础" tabindex="-1"><a class="header-anchor" href="#平台工程理论基础" aria-hidden="true">#</a> 平台工程理论基础</h3>
-<ul>
-<li><RouterLink to="/guide/bar/white-paper.html">白皮书</RouterLink></li>
-<li><RouterLink to="/guide/bar/white-paper.html">成熟度模型</RouterLink></li>
-</ul>
-</div></template>
-
-
--- src/.vuepress/.temp/theme-hope/sidebar.js
+++
@@ -1,1 +0,0 @@
-export const sidebarData = {"/demo/":["page","markdown","disable","encrypt"],"/guide/":["",{"text":"理论基础","prefix":"bar/","collapsible":true,"icon":"lightbulb","children":["","white-paper","maturity"]}]};
